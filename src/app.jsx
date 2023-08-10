1d1d5c7874e0125686e79422f7316ad25adfe3f5
Hem <hempatel1028@gmail.com>
2023-08-10 12:45:14 +0530

    Product page

---
+++ src/app.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import viteLogo from '/vite.svg'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import './app.css'
+import Product7 from './components/Product7'
+import Product8 from './components/Product8'
+import Product6 from './components/Product6'
+import Product4 from './components/Product4'
+import Product5 from './components/Product5'
+import Product3 from './components/Product3'
+import Product2 from './components/Product2'
+import RatingStar2 from './components/RatingStar2'
+import Product from './components/Product'
+import Product1 from './components/Product1'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* <Product/> */}
+    {/* <Product1/> */}
+    {/* <Product2/> */}
+    {/* <Product3/> */}
+    {/* <Product4/> */}
+   {/* <Product5/> */}
+   {/* <Product6/> */}
+   {/* <Product7/> */}
+   {/* <Product8/> */}
+    {/* <RatingStar2/> */}
+    </>
+  )
+}
